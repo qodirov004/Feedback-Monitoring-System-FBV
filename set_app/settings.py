ee59e8038c6d60411ef618c395c0b797b6311a80
Shahzodbek Qodirov <shqodirov573@gmail.com>
2025-10-18 12:46:59 +0500

    Refactor settings.py for local development
    Removed environment variable configurations for SECRET_KEY and DEBUG. Updated DATABASES configuration to use SQLite instead of dj_database_url.

--- set_app/settings.py
+++ set_app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,14 +25,9 @@ SECRET_KEY = 'django-insecure-^+minav+sx4s5eb9+%n^bt8$(^2so$rybf$=-rbqz$hl0-bcs7
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 ALLOWED_HOSTS = ["*"]
-SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-
-from decouple import config
-SECRET_KEY = config('SECRET_KEY')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
 
@@ -81,16 +75,13 @@ WSGI_APPLICATION = 'set_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
